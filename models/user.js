c548374d8c234329fb4b045141fd6b94d268d2a7
fbid <federico.bidoggia@gmail.com>
2017-06-14 18:20:52 +0200

    user schema refactoring

--- models/user.js
+++ models/user.js
@@ -3,11 +3,9 @@ var Schema = mongoose.Schema;
 var mongooseUniqueValidator = require('mongoose-unique-validator');
 
 var userSchema = new Schema({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
+    username: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true, unique: true},
-    posts: [{type: Schema.Types.ObjectId, ref: 'Post'}]
 });
 
 userSchema.plugin(mongooseUniqueValidator);
