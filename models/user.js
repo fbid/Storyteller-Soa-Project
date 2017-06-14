8dc9fff32ad18261a3b094be6096fb75ec3b7954
fbid <federico.bidoggia@gmail.com>
2017-06-14 18:27:39 +0200

    set username as unique field

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 var mongooseUniqueValidator = require('mongoose-unique-validator');
 
 var userSchema = new Schema({
-    username: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     city: {type: String, required: true},
