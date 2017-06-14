f67a0843c77a756e6fad102ca7599ee624ccfc3e
fbid <federico.bidoggia@gmail.com>
2017-06-14 18:26:22 +0200

    user schema extended

--- models/user.js
+++ models/user.js
@@ -4,6 +4,10 @@ var mongooseUniqueValidator = require('mongoose-unique-validator');
 
 var userSchema = new Schema({
     username: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    city: {type: String, required: true},
+    country: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true, unique: true},
 });
